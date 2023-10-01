524b6a56545cef3c8574c49129e145673b59b5cc
AmirRezaii <rezaiiamir52@gmail.com>
2023-10-01 23:11:43 +0330

    implemented forget password screen

---
+++ src/screens/ForgetPass.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import forgetImg from '../assets/img/forgetImg.png'
+
+const ForgetPass = () => {
+  return (
+    <div className='flex items-center justify-center mx-auto h-screen font-vazir text-dark-blue'>
+      <div className="w-full md:w-max bg-[url('assets/img/backgroundForget.png')] bg-cover flex flex-col text-center p-4 mx-6 rounded-2xl overflow-hidden">
+        <div className="image-container mt-32">
+          <img src={forgetImg} alt="" className='mx-auto' />
+        </div>
+        <p className="m-12 text-3xl font-semibold">فراموشی رمز عبور</p>
+        <input type="email" placeholder='ایمیل خود را وارد کنید' className='focus:outline-none w-full lg:w-[25rem] mx-2 my-3 px-6 py-2 shadow-md shadow-gray-200 rounded-lg placeholder-darker-green'/>
+        <button type='submit' className='bg-lighter-green/[.65] text-dark-blue/95 mx-auto mt-16 px-6 py-2 rounded-lg' >دریافت کد</button>
+        <a href="#" className='mt-10 text-darker-green underline font-semibold'>بازگشت به صفحه اصلی</a>
+      </div>
+    </div>
+  )
+}
+
+export default ForgetPass
--- src/screens/Register.jsx
+++ src/screens/Register.jsx
@@ -15,37 +15,39 @@ const Register = () => {
   }
 
   return (
-    <div className='flex items-center justify-center mx-auto h-screen font-vazir text-[rgb(50, 62, 115)]'>
+    <div className='flex items-center justify-center mx-auto h-screen font-vazir text-dark-blue'>
       <div className="w-full md:w-max bg-[url('assets/img/background.png')] bg-cover p-4 mx-6 rounded-2xl overflow-hidden">
         <div className="flex flex-col md:gap:5 lg:gap-12 md:flex-row mx-6">
           <div className="flex flex-col justify-center text-center min-w-[15rem]">
-            <div className="bg-[rgba(130,216,185,.65)] flex flex-row gap-4 mx-auto my-4 px-2 py-2 rounded-3xl">
+            <div className="bg-lighter-green/[.65] flex flex-row gap-4 mx-auto my-4 px-2 py-2 rounded-3xl">
               <a href='register' className='bg-white shadow-md shadow-gray-300 px-5 py-1 rounded-2xl'>ثبت نام</a>
               <a href='login' className='px-5 py-1 rounded-2xl'>ورود کاربر</a>
             </div>
             <p className="m-9 text-xl">حساب کاربری خود را ایجاد کنید</p>
             <div className="relative">
-              <input type="email" placeholder='ایمیل' className='focus:outline-none w-full lg:w-96 mx-2 my-3 px-6 py-2 shadow-md shadow-gray-200 rounded-lg' />
+              <input type="email" placeholder='ایمیل' className='focus:outline-none w-full lg:w-[25rem] mx-2 my-3 px-6 py-2 shadow-md shadow-gray-200 rounded-lg placeholder-darker-green' />
             </div>
             <div className='relative'>
-              <input type={passVisible1?'text':'password'} placeholder='رمز' className='focus:outline-none w-full lg:w-[25rem] mx-2 my-3 px-6 py-2 shadow-md shadow-gray-200 rounded-lg' />
+              <input type={passVisible1?'text':'password'} placeholder='رمز' className='focus:outline-none w-full lg:w-[25rem] mx-2 my-3 px-6 py-2 shadow-md shadow-gray-200 rounded-lg placeholder-darker-green' />
               <span onClick={togglePass1} className='absolute left-4 top-1/2 -translate-y-1/2'>
                 <img src={passVisible1?closedEye:openEye} alt="" className='w-6 h-6' />
               </span>
             </div>
             <div className='relative'>
-              <input type={passVisible2?'text':'password'} placeholder='تکرار رمز' className='focus:outline-none w-full lg:w-[25rem] mx-2 my-3 px-6 py-2 shadow-md shadow-gray-200 rounded-lg' />
+              <input type={passVisible2?'text':'password'} placeholder='تکرار رمز' className='focus:outline-none w-full lg:w-[25rem] mx-2 my-3 px-6 py-2 shadow-md shadow-gray-200 rounded-lg placeholder-darker-green' />
               <span onClick={togglePass2} className='absolute left-4 top-1/2 -translate-y-1/2'>
                 <img src={passVisible2?closedEye:openEye} alt="" className='w-6 h-6' />
               </span>
             </div>
             <button type='submit' className='bg-[rgba(130,216,185,.65)] w-24 mx-auto mt-12 py-2 rounded-lg' >ثبت نام</button>
-            <div className='flex justify-between mt-5 text-[rgb(130,216,185)] underline font-semibold'>
+            <div className='flex justify-between mt-5 text-darker-green underline font-semibold'>
               <a href="#">بازگشت به صفحه اصلی</a>
               <a href="#">ورود کارمندان</a>
             </div>
           </div>
-          <img src={registerImg} alt="image not found" className='hidden md:scale-75 lg:scale-100 md:block object-fill' />
+          <div className="image-container shrink-0">
+            <img src={registerImg} alt="image not found" className='hidden md:scale-75 lg:scale-100 md:block' />
+          </div>
         </div>
       </div>
     </div>
