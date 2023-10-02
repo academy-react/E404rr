9050aa5731f8958aae59ef3447f8edfb6072d3f5
pilowmaningit <kolafmob@gmail.com>
2023-10-02 05:04:36 +0330

    basic skeleton part 2

---
+++ src/components/LandingTop/Items/Item.jsx
@@ -0,0 +1,20 @@
+const Item = () => {
+  return (
+        <>
+            <div className="w-71.875 h-80.325 border">
+              <div className="h-45">
+                <img src="" alt="" />
+                img
+              </div>
+              <div className="">
+                <h3>آموزش جامع زبان پایتون از صفر تا صد</h3>
+                <p>حسامی</p>
+                <span>زبان </span>
+                <span>قیمت   </span>
+              </div>
+            </div>
+        </>
+  )
+}
+
+export  {Item}
