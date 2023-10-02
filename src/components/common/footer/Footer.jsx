b418a5e7fe76dd80a2c990c8d531ae060a605a0a
MahdyarShirzad <139555819+MahdyarShirzad@users.noreply.github.com>
2023-10-02 23:05:37 +0330

    some styles optimized and address added to footer

--- src/components/common/footer/Footer.jsx
+++ src/components/common/footer/Footer.jsx
@@ -1,20 +1,21 @@
 import Access from "./Access";
+import Address from "./Address";
+import Brand from "./Brand";
 import Common from "./Common";
 import Contact from "./Contact";
 import Trust from "./Trust";
-import style from "./footer.module.css";
 
 const Footer = () => {
   return (
-    <div className={style.footerContainer}>
-      <footer
-        className={` ${style.footer} container-xl flex mx-auto items-stretch	justify-between	 pt-8`}
-      >
+    <div className="bg-[#3c6e71]">
+      <footer className="container-xl flex mx-auto items-stretch	justify-between	 pt-8 max-w-[1366px] h-[420px] text-[#f2fcf8]">
         <Access />
         <Common />
         <Contact />
         <Trust />
       </footer>
+      <Address />
+      <Brand />
     </div>
   );
 };
