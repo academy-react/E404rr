765790f232a32707966ac39084b05cd63248ab9f
MahdyarShirzad <139555819+MahdyarShirzad@users.noreply.github.com>
2023-10-13 23:21:55 +0330

    article details page created

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,11 +1,12 @@
-import { useState } from 'react'
+import ArticleDetails from "../screens/ArticleDetails/ArticleDetails";
 
 function App() {
-
   return (
     <>
+      <div className="text-center my-10 border-2">Header</div>
+      <ArticleDetails />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
