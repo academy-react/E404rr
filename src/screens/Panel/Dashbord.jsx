d5ae4ae2c2b1195db8c21d855a666eb537ef3ebe
pilowmaningit <kolafmob@gmail.com>
2023-10-25 02:16:11 +0330

    add

--- src/screens/Panel/Dashbord.jsx
+++ src/screens/Panel/Dashbord.jsx
@@ -5,7 +5,7 @@ const Dashbord = () => {
     <div className='border border-transparent shadow-xl w-[955px] h-[722px] mt-[23px] mr-[34px] bg-[#f3fcf8] rounded-lg'>
          <h3 className='text-gray-600 text-[22px] before:content-["*"] before:text-green-600  mt-[18px] mr-4' >داشبورد</h3>
 
-         <div className='flex  borde border-transparent mt-5'>
+         <div className='flex  border border-transparent mt-5'>
             <div className='bg-white border w-[400px] h-[150px] border-transparent rounded mr-16 shadow'></div>
             <div className='bg-white border w-[400px] h-[150px]  border-transparent rounded mr-10 shadow'></div>
          </div>
