65be603f8af9ad7b6834098ec57197e03a67632d
MahdyarShirzad <139555819+MahdyarShirzad@users.noreply.github.com>
2023-10-27 14:21:44 +0330

    site intro components fixed

--- src/components/LandingTop/SiteIntro.jsx
+++ src/components/LandingTop/SiteIntro.jsx
@@ -3,21 +3,26 @@ import { TypeAnimation } from "react-type-animation";
 
 const SiteIntro = () => {
   return (
-    <>
+    <div className="h-20">
       <TypeAnimation
-        sequence={["آموزش برنامه نویسی ، خودآموزی ، ورود به بازار کار "]}
+        sequence={[
+          // Same substring at the start will only be typed out once, initially
+          1000,
+          "آموزش برنامه نویسی ، خودآموزی ، ورود به بازارکار ",
+        ]}
         wrapper="h2"
         speed={50}
         // style={styles}
         repeat="0"
-        className="text-gray-600 text-[24px] mt-10 xl:mr-16 md:mr-[0] mr-16 lg:mr-[260px]"
+        className="text-gray-600 text-[24px] mt-10 xl:mr-16 md:mr-[0] mr-16 lg:mr-[260px] "
         cursor={false}
       />
       <TypeAnimation
-        sequence={([""], 4000, ["حرفه ایی شدن رو از امروز شروع کن"])}
+        sequence={[" ", 4000, "حرفه ایی شدن رو از امروز شروع کن"]}
         wrapper="h2"
         speed={50}
         // style={styles}
+        del
         repeat="0"
         className="mt-4 mr-[142px] md:mr-[0px] xl:mr-[142px] lg:mr-[260px] text-[#323e73] font-thin text-[20px]"
         cursor={false}
@@ -28,7 +33,7 @@ const SiteIntro = () => {
           <p className=" mt-4 mr-[142px] md:mr-[0px] xl:mr-[142px] lg:mr-[260px] text-gray-500 text-[21px]">
             حرفه ای شدن را از امروز شروع کن
           </p> */}
-    </>
+    </div>
   );
 };
 
