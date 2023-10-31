236657a3bc767e69af002fa2a8788ee825a1f8b9
MahdyarShirzad <139555819+MahdyarShirzad@users.noreply.github.com>
2023-10-31 16:38:26 +0330

    .

--- src/components/common/CourseCard/CourseCard.jsx
+++ src/components/common/CourseCard/CourseCard.jsx
@@ -1,29 +1,25 @@
 import CourseImg from "./CourseImg";
 import CourseInfo from "./CourseInfo";
 import CourseDetails from "./CourseDetails";
-import {
-  useNavigate,
-} from "react-router-dom";
-
+import { useNavigate } from "react-router-dom";
 
 import styles from "./CourseCard.module.css";
 
 const CourseCard = () => {
-
-    const navigate = useNavigate();
-    const goCourse = () =>{
-        navigate("/Course")
-      }
+  const navigate = useNavigate();
+  const goCourse = () => {
+    navigate("/Course");
+  };
   return (
-      <button onClick={goCourse}>
-            <div 
-      className={`${styles.courseCard} hover:scale-105 cursor-pointer   w-[287.5px] h-[321.3px] max-lg:mx-10 max-xl:my-10 rounded mb-8 `}
-    >
-      <CourseImg />
-      <CourseInfo />
-      <CourseDetails />
-    </div>
-      </button>
+    <button onClick={goCourse}>
+      <div
+        className={`${styles.courseCard} text-right hover:scale-105 cursor-pointer   w-[287.5px] h-[321.3px] max-lg:mx-10 max-xl:my-10 rounded mb-8 `}
+      >
+        <CourseImg />
+        <CourseInfo />
+        <CourseDetails />
+      </div>
+    </button>
   );
 };
 
