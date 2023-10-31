38ffdcbfbd9ad6c86bdb7f3f9bf0c085d4d93225
pilowman <kolafmob@gmail.com>
2023-10-31 12:08:21 +0330

    ++set Change

--- src/screens/Courses/CategoryFilter.jsx
+++ src/screens/Courses/CategoryFilter.jsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Courses.module.css";
 import CategoryItem from "./CategoryItem";
 
 const CategoryFilter = () => {
+  const [count,setCount] = useState(1);
+  const [CategoryList, setCategoryList] = useState([
+    { id : "01" , HtmlFor : "01" , Name : "ری اکت"  },
+    { id : "02" , HtmlFor : "02"  , Name : "پایتون"},
+    { id : "03" , HtmlFor : "03"   , Name : "سی اس اس"},
+    { id : "04" , HtmlFor : "04"  , Name : "فلاتر" },
+    { id : "05" , HtmlFor : "05"  , Name : "سی شارپ" },
+    { id : "06" , HtmlFor : "06" , Name : "سی پلاس پلاس" },
+
+  
+  ]);
   return (
     <div
       className={` ${styles.categoryFilter} rounded-lg  text-[#323E73] mt-10 py-3 px-5`}
     >
       <p className="text-lg font-bold mb-3">دسته بندی</p>
-      <CategoryItem  />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
-      <CategoryItem />
+      {CategoryList.map((item , index) => {
+        return(
+          <CategoryItem  key={index} id={item.id} HtmlFor={item.HtmlFor} Name={item.Name}/>
+        )
+      })}
+
+
     </div>
   );
 };
--- src/screens/Courses/CategoryItem.jsx
+++ src/screens/Courses/CategoryItem.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-const CategoryItem = () => {
+const CategoryItem = ({id , HtmlFor , Name}) => {
   return (
     <div className="flex items-center my-2 text-[#323E73]">
-      <input id="react-check" className="checked:accent-[#198643]" type="checkbox" />
-      <label htmlFor="react-check" className="mr-2">ری اکت</label>
+      <input id={id} className="checked:accent-[#198643]" type="checkbox" />
+      <label htmlFor={HtmlFor} className="mr-2">{Name} </label>
     </div>
   );
 };
