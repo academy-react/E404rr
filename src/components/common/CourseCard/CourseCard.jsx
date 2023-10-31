1bb47d45c2edc4316c51508e8c6094849f6a1914
pilowman <kolafmob@gmail.com>
2023-10-31 16:34:27 +0330

    AddgoCurse

--- src/components/common/CourseCard/CourseCard.jsx
+++ src/components/common/CourseCard/CourseCard.jsx
@@ -1,18 +1,29 @@
 import CourseImg from "./CourseImg";
 import CourseInfo from "./CourseInfo";
 import CourseDetails from "./CourseDetails";
+import {
+  useNavigate,
+} from "react-router-dom";
+
 
 import styles from "./CourseCard.module.css";
 
 const CourseCard = () => {
+
+    const navigate = useNavigate();
+    const goCourse = () =>{
+        navigate("/Course")
+      }
   return (
-    <div
-      className={`${styles.courseCard} hover:scale-105 cursor-pointer  w-[287.5px] h-[321.3px] max-lg:mx-10 max-xl:my-10 rounded mb-8 `}
+      <button onClick={goCourse}>
+            <div 
+      className={`${styles.courseCard} hover:scale-105 cursor-pointer   w-[287.5px] h-[321.3px] max-lg:mx-10 max-xl:my-10 rounded mb-8 `}
     >
       <CourseImg />
       <CourseInfo />
       <CourseDetails />
     </div>
+      </button>
   );
 };
 
