5b4ddaf626fe60e4e101e348ad084d4a361084ad
pilowman <kolafmob@gmail.com>
2023-11-04 15:55:40 +0330

    AddPageOne

--- src/screens/Panel2/PishKhan.jsx
+++ src/screens/Panel2/PishKhan.jsx
@@ -6,6 +6,16 @@ const PishKhan = () => {
   return (
         <>
             <PishKhanItems/>
+            <div className='w-[461px] h-[140px] border border-transparent rounded-xl  mr-[30px] mt-[30px] bg-white  '>
+
+                    <div className='w-[90%] border-b mx-auto'>
+                       <h3 className='text-[20px] mt-[15px] pb-4'>پرسش های اخیر</h3>
+                        
+                    </div>
+                    <div className='flex  justify-center items-center border border-transparent  h-[77px]'>
+                      <p>تا به حال پرسشی ارسال نکردید!</p>
+                    </div>
+            </div>
             
         </>
   )
