1f9e5fc07310e2954a2a521893c7ca830aed39a5
pilowman <kolafmob@gmail.com>
2023-11-25 05:41:12 +0330

    Add3last

--- src/screens/ArticleDetails/ArticlesLast.jsx
+++ src/screens/ArticleDetails/ArticlesLast.jsx
@@ -1,16 +1,35 @@
 import React from "react";
 import LastArticleCard from "./LastArticleCard";
+import { useState } from "react";
+import { getAllNews } from "../../core/services/api/AllNews";
+import { useEffect } from "react";
 
 const ArticlesLast = () => {
+  const [lastNews , setLastNews] = useState([])
+
+
+  const getList = async () => {
+    const courses = await getAllNews();
+    setLastNews(courses.slice(-3))  
+  } 
+  useEffect(() =>{
+      getList();
+  },[]);
   return (
     <div     data-aos="fade-up">
       <div className="flex items-center text-white bg-[#23A455] text-xl max-w-[300px] font-bold py-4 px-6 rounded-3xl" data-aos="fade-up">
         آخرین مقالات
       </div>
       <div className="mt-5 bg-gradient-to-bl from-[#f3fcf8] to-white rounded-3xl px-6 py-4 max-w-[300px] text-[#323E73]">
-        <LastArticleCard />
-        <LastArticleCard />
-        <LastArticleCard />
+        {/* <LastArticleCard /> */}
+
+        {lastNews.map((item , index) => {
+            return(
+            <>
+                 <LastArticleCard  id={item.id} title={item.title} insertDate={item.insertDate} /> 
+            </>)
+        })}
+
       </div>
     </div>
   );
--- src/screens/ArticleDetails/LastArticleCard.jsx
+++ src/screens/ArticleDetails/LastArticleCard.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import img from "../../assets/img/Landing/02.png";
+import { NavLink } from "react-router-dom";
 
-const LastArticleCard = () => {
+const LastArticleCard = ({id , title , insertDate}) => {
   return (
     <div className="flex items-center justify-between py-3 my-2">
       <div className="w-24">
         <img className="rounded-2xl" src={img} alt="" />
       </div>
-      <div>
-        <p className="text-lg font-bold flex flex-nowrap">الگوریتم چیست</p>
-        <p className="text-sm">10 خرداد 1399</p>
-      </div>
+      {/* <div> */}
+          <NavLink to={`/ArticalePage/${id}`}>
+          {/* <p className="text-lg font-bold flex flex-nowrap">الگوریتم چیست</p> */}
+          <p className="text-lg font-bold flex flex-nowrap">{title.slice(length , 16)}...</p>
+
+        {/* <p className="text-sm">10 خرداد 1399</p> */}
+        <p className="text-sm">{insertDate.slice(length , 12)}</p>
+          </NavLink>
+      {/* </div> */}
     </div>
   );
 };
